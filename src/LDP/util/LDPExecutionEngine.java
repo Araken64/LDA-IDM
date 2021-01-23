5ff6bdf45db4267cbb90701bfc4ea2ff573dfee7
Araken64 <elian.araken@gmail.com>
2021-01-23 20:58:47 +0100

    First cycle of Execute

--- src/LDP/util/LDPExecutionEngine.java
+++ src/LDP/util/LDPExecutionEngine.java
@@ -3,23 +3,29 @@ package LDP.util;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LDPExecutionEngine {
-	public static void execute(String fileName, Object target, HashMap tags) {
+	public static void execute(String fileName, Object target, HashMap tags) throws Exception {
+		LDPManipulation ldp = new LDPManipulation();
+		LDP.Processus processus = ldp.getProcessus(fileName);
+		LDP.Activite firstActivity = processus.getDebut().getReference();
+		processus.setActiviteCourante(firstActivity);
+		LDP.Operation operation = firstActivity.getAction();
 		
-		// LDP.Activite firstAct = target.getDebut().getReference();
-		// target.setActiviteCourante(firstAct);
-		// LDP.Operation op = firstAct.getAction();
+		List<String> paramsName = operation.getParamsTag();
+		Object[] params = paramsName.stream().map(name -> tags.get(name)).toArray();
+		Object result = dynamicInvoke(operation.getMethodName(), target, params);
 		
-		//if(op != null) {
-		// 	String name = op.getMethodName();
-		//	List<String> params = op.getParamsTag();
-		//}
-		LDP.Processus proc;
-
+		LDP.Activite lastActivity = processus.getFin().getReference();
+		
+		LDP.Activite nextActivity = firstActivity.getSuivante();
+		while(nextActivity != null) {
+			
+		}
 	}
 	
-	public Object dynamicInvoke(String methodName, Object target, Object params[]) throws Exception {
+	public static Object dynamicInvoke(String methodName, Object target, Object params[]) throws Exception {
 		   Class cl = target.getClass();
 		   Class[] paramsClass = new Class[params.length];
 		   for (int i=0; i < params.length; i++)
