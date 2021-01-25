8be5d313310a6932c6616d0720b55456d049f935
Araken64 <elian.araken@gmail.com>
2021-01-25 18:25:07 +0100

    Finish execution system + exception management

--- src/LDP/util/LDPExecutionEngine.java
+++ src/LDP/util/LDPExecutionEngine.java
@@ -3,35 +3,51 @@ package LDP.util;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LDPExecutionEngine {
-	public static void execute(String fileName, Object target, HashMap tags) throws Exception {
+	public static void execute(String fileName, Object target, HashMap tags) throws LDPEngineException {
 		LDPManipulation ldp = new LDPManipulation();
 		LDP.Processus processus = ldp.getProcessus(fileName);
-		LDP.Activite firstActivity = processus.getDebut().getReference();
-		processus.setActiviteCourante(firstActivity);
-		LDP.Operation operation = firstActivity.getAction();
-		
-		List<String> paramsName = operation.getParamsTag();
-		Object[] params = paramsName.stream().map(name -> tags.get(name)).toArray();
-		Object result = dynamicInvoke(operation.getMethodName(), target, params);
-		
-		LDP.Activite lastActivity = processus.getFin().getReference();
-		
-		LDP.Activite nextActivity = firstActivity.getSuivante();
-		while(nextActivity != null) {
-			
+		if (processus == null) throw new LDPEngineException("Processus was not found in model", new Exception());
+		LDP.Activite currentActivity = processus.getDebut().getReference(); // get first Activity
+
+		LDP.Operation operation;
+		List<String> paramsName;
+		Object[] params;
+		Object result;
+				
+		while(currentActivity != null) { // while last activity have not been executed
+			processus.setActiviteCourante(currentActivity);
+			operation = currentActivity.getAction();
+			paramsName = operation.getParamsTag();
+			params = paramsName.stream().map(name -> tags.get(name)).toArray();
+			try {
+				result = dynamicInvoke(operation.getMethodName(), target, params);
+			} catch(Exception e) {
+				throw new LDPEngineException("Dynamic invoke failed : check method name or params", e);
+			}
+			tags.put(operation.getReturnTag(), result);
+			currentActivity = currentActivity.getSuivante();
 		}
 	}
 	
 	public static Object dynamicInvoke(String methodName, Object target, Object params[]) throws Exception {
-		   Class cl = target.getClass();
-		   Class[] paramsClass = new Class[params.length];
-		   for (int i=0; i < params.length; i++)
-		      paramsClass[i] = params[i].getClass();
-		   Method met = cl.getMethod(methodName, paramsClass);
-		   Object result = met.invoke(target, params);
-		   return result;
-		}
+	   Class cl = target.getClass();
+	   Class[] paramsClass = new Class[params.length];
+	   for (int i=0; i < params.length; i++)
+	      paramsClass[i] = params[i].getClass();
+	   Method met = cl.getMethod(methodName, paramsClass);
+	   Object result = met.invoke(target, params);
+	   return result;
+	}
+}
+
+final class LDPEngineException extends RuntimeException {
+	public LDPEngineException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
+    }
+	
+	public LDPEngineException(String errorMessage) {
+        super(errorMessage);
+    }
 }
--- src/LDPparallel/util/Calcul.java
+++ src/LDPparallel/util/Calcul.java
@@ -1,3 +1,4 @@
+package LDPparallel.util;
 
 public class Calcul {
 	
