f8b8fa70d3b5357e705eb56dec192187ead53718
Araken64 <elian.araken@gmail.com>
2021-01-26 20:05:41 +0100

    class for shared functions

---
+++ src/common/ModelHelper.java
@@ -0,0 +1,57 @@
+package common;
+
+import java.lang.reflect.Method;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLMapImpl;
+
+public class ModelHelper {
+	public static Resource chargerModele(String uri, EPackage pack) {
+		Resource resource = null;
+		try {
+			URI uriUri = URI.createURI(uri);
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+			resource = (new ResourceSetImpl()).createResource(uriUri);
+			XMLResource.XMLMap xmlMap = new XMLMapImpl();
+			xmlMap.setNoNamespacePackage(pack);
+			java.util.Map options = new java.util.HashMap();
+			options.put(XMLResource.OPTION_XML_MAP, xmlMap);
+			resource.load(options);
+		}
+		catch(Exception e) {
+			System.err.println("ERREUR chargement du modèle : "+e);
+			e.printStackTrace();
+		}
+		return resource;
+	}
+	
+	public static void sauverModele(String uri, EObject root) {
+		Resource resource = null;
+		try {
+			URI uriUri = URI.createURI(uri);
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+			resource = (new ResourceSetImpl()).createResource(uriUri);
+			resource.getContents().add(root);
+			resource.save(null);
+		} catch (Exception e) {
+			System.err.println("ERREUR sauvegarde du modèle : "+e);
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object dynamicInvoke(String methodName, Object target, Object params[]) throws Exception {
+	   Class cl = target.getClass();
+	   Class[] paramsClass = new Class[params.length];
+	   for (int i=0; i < params.length; i++)
+	      paramsClass[i] = params[i].getClass();
+	   Method met = cl.getMethod(methodName, paramsClass);
+	   Object result = met.invoke(target, params);
+	   return result;
+	}
+}
