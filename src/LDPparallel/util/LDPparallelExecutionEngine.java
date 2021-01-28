13da41f50a1ccad41b6612939971e2c76ff7e434
Araken64 <elian.araken@gmail.com>
2021-01-28 16:25:29 +0100

    Clean code and functions organization

--- src/LDPparallel/util/LDPparallelExecutionEngine.java
+++ src/LDPparallel/util/LDPparallelExecutionEngine.java
@@ -27,8 +27,60 @@ public class LDPparallelExecutionEngine {
 	public void execute(String fileName, Object target, HashMap tags) throws LDPparallelEngineException {
 		processus = LDPparallelManipulation.getProcessus(fileName);
 		if (processus == null) throw new LDPparallelEngineException("Processus was not found in model");
-		Debut debut = processus.getDebut(); // TODO case if already active sequences at inital state
+		Debut debut = processus.getDebut();
 		runDebut(debut, target, tags);
+	}
+	
+	private void runDebut(Debut debut, Object target, HashMap tags) {
+		new Thread(() -> runElementProcessus(debut.getReference(), target, tags)).start();
+	}
+	
+	private void runElementProcessus(ElementProcessus element, Object target, HashMap tags) {
+		if (element instanceof Sequence) {
+			runSequence((Sequence) element, target, tags);
+			// if Fin references this ElementProcessus
+			if(element == processus.getFin().getReference()) new Thread(() -> runFin(processus.getFin(), target, tags)).start();
+			// if a Porte has this ElementProcessus as predecessor
+			Stream<Porte> portesSuivantes = processus.getPortes().stream().filter(porte -> isPredecessor(element, porte));
+			portesSuivantes.forEach(porte -> new Thread(() -> runPorte(porte, target, tags)).start());
+		} else if (element instanceof Porte) {
+			runPorte((Porte) element, target, tags);
+		} else { // last case : ElementProcessus is a PseudoEtat
+			runPseudoEtat((PseudoEtat) element, target, tags);
+		}
+	}
+	
+	private void runSequence(Sequence sequence, Object target, HashMap tags) {		
+		Operation operation;
+		List<String> paramsName;
+		Object[] params;
+		Object result;
+		
+		Activite currentActivity = sequence.getPremiereActivite();
+		
+		while(currentActivity != null) { // while last activity have not been executed
+			sequence.setActiviteCourante(currentActivity);
+			operation = currentActivity.getAction();
+			paramsName = operation.getParamsTag();
+			params = paramsName.stream().map(name -> tags.get(name)).toArray(); // get tags values required
+			try {
+				result = ModelHelper.dynamicInvoke(operation.getMethodName(), target, params);
+			} catch(Exception e) {
+				throw new LDPparallelEngineException("Dynamic invoke failed : check method name or params", e);
+			}
+			tags.put(operation.getReturnTag(), result);
+			currentActivity = currentActivity.getSuivante();
+		}
+		sequence.setActiviteCourante(null);
+	}
+	
+	
+	private void runPorte(Porte porte, Object target, HashMap tags) {
+		if(porte instanceof Fourche) {
+			runFourche((Fourche) porte, target, tags);
+		} else {
+			runJonction((Jonction) porte, target, tags);
+		}
 	}
 	
 	private void runFourche(Fourche fourche, Object target, HashMap tags) {
@@ -39,87 +91,23 @@ public class LDPparallelExecutionEngine {
 	
 	// synchronized to protect verifPreJonction access
 	private synchronized void runJonction(Jonction jonction, Object target, HashMap tags) {
-		verifPreJonction.putIfAbsent(jonction, jonction.getPred().size());
-		verifPreJonction.replace(jonction, verifPreJonction.get(jonction) - 1);
-		System.out.println(verifPreJonction.get(jonction));
-		if(verifPreJonction.get(jonction) == 0) {
+		verifPreJonction.putIfAbsent(jonction, jonction.getPred().size()); // number of predecessor required
+		verifPreJonction.replace(jonction, verifPreJonction.get(jonction) - 1); // decrease for each predecessor reach
+		if(verifPreJonction.get(jonction) == 0) { // wait for the last thread to reach the Jonction
 			new Thread(() -> runElementProcessus(jonction.getSucc(), target, tags)).start();
-		}
-	}
-	
-	private void runPorte(Porte porte, Object target, HashMap tags) {
-		if(porte instanceof Fourche) {
-			runFourche((Fourche) porte, target, tags);
-		} else {
-			runJonction((Jonction) porte, target, tags);
-		}
-	}
-	
-	private void runElementProcessus(ElementProcessus element, Object target, HashMap tags) {
-		if (element instanceof Porte) {
-			System.out.println("BeginElement : Porte");
-			runPorte((Porte) element, target, tags);
-			System.out.println("FinishElement : Porte");
-		} else if (element instanceof Sequence) {
-			System.out.println("BeginElement : Sequence -> " + ((Sequence) element).getName());
-			runSequence((Sequence) element, target, tags);
-			System.out.println("FinishElement : Sequence -> " + ((Sequence) element).getName());
-			
-			if(element == processus.getFin().getReference()) {
-				new Thread(() -> runFin(processus.getFin(), target, tags)).start();
-			}
-			Stream<Porte> portesSuivantes = processus.getPortes().stream().filter(porte -> isPredecessor(element, porte));
-			portesSuivantes.forEach(porte -> {
-				new Thread(() -> runPorte(porte, target, tags)).start();
-			});
-		} else {
-			System.out.println("BeginElement : PseudoEtat");
-			runPseudoEtat((PseudoEtat) element, target, tags);
-			System.out.println("FinishElement : PseudoEtat");
 		}
 	}
 
 	private void runPseudoEtat(PseudoEtat etat, Object target, HashMap tags) {
 		if(etat instanceof Debut) {
 			runDebut((Debut) etat, target, tags);
-		} else {
+		} else { // last case : PseudoEtat is Fin
 			runFin((Fin) etat, target, tags);
 		}
 	}
 	
-	private void runDebut(Debut debut, Object target, HashMap tags) {
-		System.out.println("BeginElement : Debut");
-		new Thread(() -> runElementProcessus(debut.getReference(), target, tags)).start();
-		System.out.println("FinishElement : Debut");
-	}
-	
 	private void runFin(Fin fin, Object target, HashMap tags) {
-		System.out.println("Fin execution");
 		System.out.println(tags);
-	}
-	
-	private void runSequence(Sequence sequence, Object target, HashMap tags) {		
-		Operation operation;
-		List<String> paramsName;
-		Object[] params;
-		Object result;
-		
-		Activite currentActivity = sequence.getPremiereActivite(); // TODO case if already current activity
-		
-		while(currentActivity != null) { // while last activity have not been executed
-			sequence.setActiviteCourante(currentActivity);
-			operation = currentActivity.getAction();
-			paramsName = operation.getParamsTag();
-			params = paramsName.stream().map(name -> tags.get(name)).toArray();
-			try {
-				result = ModelHelper.dynamicInvoke(operation.getMethodName(), target, params);
-			} catch(Exception e) {
-				throw new LDPparallelEngineException("Dynamic invoke failed : check method name or params", e);
-			}
-			tags.put(operation.getReturnTag(), result);
-			currentActivity = currentActivity.getSuivante();
-		}
-		sequence.setActiviteCourante(null);
 	}
 	
 	private static boolean isPredecessor(ElementProcessus element, Porte porte) {
